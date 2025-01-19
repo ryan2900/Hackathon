e815935c1fa4839d9526ed6b4a7c2cc45253873a
Darrenk328 <dmkapturski@gmail.com>
2025-01-18 22:22:39 -0500

    Darren's Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+x = 400
+y = 400
+speed = 1
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  rect(width/2, height/2, 50, 50)
+  if(x >= 800 && y >= 800){
+    x = 400
+    y = 400)
+  }
 }
